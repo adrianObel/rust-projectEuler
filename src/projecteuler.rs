50729d35840d8f1691fb1656d41109ae1636b223
Adrian Obelmejias <obeladrian@gmail.com>
2014-07-05 23:27:24 -0430

    Problem 002

--- src/projecteuler.rs
+++ src/projecteuler.rs
@@ -1,13 +1,17 @@
 use std::os;
 
+mod prob1;
+mod prob2;
+
 fn main() {
     let arguments = os::args();
     let arguments = arguments.iter().map(|x| x.as_slice())
         .collect::<Vec<&str>>();
 
-    match arguments.as_slice() {
-        [_, "--prob", "001"] => println!("Solve problem 001"),
-        _ => ()
-    }
+    println!("Answer is: {}", match arguments.as_slice() {
+        [_, "--prob", "001"] => prob1::solve(),
+        [_, "--prob", "002"] => prob2::solve(),
+        _ => fail!("Problem not resolved")
+    });
 
 }
