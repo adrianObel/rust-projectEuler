d7d45f0e2d5ef2b612540794bb460c9e83cf229a
Adrian Obelmejias <obeladrian@gmail.com>
2014-07-05 23:27:06 -0430

    Problem 001

--- src/prob1.rs
+++ src/prob1.rs
@@ -1,9 +1,8 @@
 use std::iter::AdditiveIterator;
 
-pub mod prob1 {
-    pub fn solve()
-        println!("Answer: {}", range(0u, 1000)
-            .filter(|&x| x % 3 == 0 || x % 5 == 0)
-            .sum());
-    }
+pub fn solve() -> String {
+    range(0u, 1000)
+        .filter(|&x| x % 3 == 0 || x % 5 == 0)
+        .sum()
+        .to_str()
 }
