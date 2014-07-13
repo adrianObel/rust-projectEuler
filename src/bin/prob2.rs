2034c638e3a0445b3bbb902e9a338c895f42ec6f
Adrian Obelmejias <obeladrian@gmail.com>
2014-07-12 21:59:07 -0430

    Imported only the function

--- src/bin/prob2.rs
+++ src/bin/prob2.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::mem::replace;
 use std::iter::AdditiveIterator;
 
 fn print_question() {
@@ -16,9 +16,9 @@ struct Fibonacchi {
 impl Iterator<uint> for Fibonacchi {
     fn next(&mut self) -> Option<uint> {
         let new_next = self.curr + self.next;
-        let new_curr = mem::replace(&mut self.next, new_next);
+        let new_curr = replace(&mut self.next, new_next);
 
-        Some(mem::replace(&mut self.curr, new_curr))
+        Some(replace(&mut self.curr, new_curr))
     }
 }
 
@@ -33,5 +33,5 @@ pub fn solve() -> uint {
         .take_while(|&x| x < 4_000_000)
         .filter(|&x| x % 2 == 0)
         .sum()
-    
+
 }
