e917c3191a2b42f08595739697e77b6e93369cec
Adrian Obelmejias <obeladrian@gmail.com>
2014-07-13 13:42:23 -0430

    Moved fib code to lib

--- src/bin/prob2.rs
+++ src/bin/prob2.rs
@@ -1,5 +1,7 @@
-use std::mem::replace;
+extern crate math;
+
 use std::iter::AdditiveIterator;
+use self::math::fibonacci::Fibonacci;
 
 fn print_question() {
     println!("Problem #2");
@@ -8,28 +10,12 @@ fn print_question() {
     println!("do not exceed four million, find the sum of the even-valued terms.\n");
 }
 
-struct Fibonacchi {
-    curr: uint,
-    next: uint
-}
-
-impl Iterator<uint> for Fibonacchi {
-    fn next(&mut self) -> Option<uint> {
-        let new_next = self.curr + self.next;
-        let new_curr = replace(&mut self.next, new_next);
-
-        Some(replace(&mut self.curr, new_curr))
-    }
-}
-
-fn fibonacchi() -> Fibonacchi {
-    Fibonacchi { curr: 1, next: 1 }
-}
-
 pub fn solve() -> uint {
     print_question();
 
-    fibonacchi()
+    let series = Fibonacci::new();
+
+    series
         .take_while(|&x| x < 4_000_000)
         .filter(|&x| x % 2 == 0)
         .sum()
